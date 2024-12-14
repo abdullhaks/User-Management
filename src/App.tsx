d29ef77522e5b53b015ab83ca5be4df561fd44d8
abdullhaks <aks4u786@gmail.com>
2024-12-14 17:22:29 +0530

    routes and pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
+import Profile from "./Pages/Profile"
+import About from "./Pages/About"
 
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
