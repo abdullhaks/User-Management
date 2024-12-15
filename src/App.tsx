9949cf7e852e30468603b46cf3a9894875b674e9
abdullhaks <aks4u786@gmail.com>
2024-12-15 07:22:12 +0530

    set header and links

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import SignIn from "./Pages/SignIn"
 import SignUp from "./Pages/SignUp"
 import Profile from "./Pages/Profile"
 import About from "./Pages/About"
+import Header from "./Components/Header"
 
 
 function App() {
   return (
     <BrowserRouter>
+        <Header />
       <Routes>
+
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
+
       </Routes>
     </BrowserRouter>
   );
